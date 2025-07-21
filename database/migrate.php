01c8bbacc5cfb6d71e9a050c5db73511a7456968
1cbyc <isaacnsisong@gmail.com>
2025-07-21 02:37:21 -0700

    added db migration script

---
+++ database/migrate.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/bootstrap.php';
+
+$migrations = glob(__DIR__ . '/migrations/*.php');
+foreach ($migrations as $migration) {
+    require $migration;
+}
+echo "Migrations completed\n"; 
