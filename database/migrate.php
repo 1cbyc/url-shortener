37a6d4691e289207bc451161e05be0a62d75eec8
1cbyc <isaacnsisong@gmail.com>
2025-07-21 04:37:23 -0700

    sorry i changed the pc i used to start this so i need to setup anew

--- database/migrate.php
+++ database/migrate.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/bootstrap.php';
-
+ 
 $migrations = glob(__DIR__ . '/migrations/*.php');
 foreach ($migrations as $migration) {
     require $migration;
